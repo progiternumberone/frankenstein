029fa6b43cd64e6418047d348d7177d8de7eb265
JK <proemailernumberone@gmail.com>
2024-05-22 15:18:29 -0500

    fixing paths

--- frankenstein/application/igor/js/frankenstein-config.js
+++ frankenstein/application/igor/js/frankenstein-config.js
@@ -2,5 +2,5 @@ var FrankensteinConfig = {};
 //FrankensteinConfig.root = "/frankenstein/application/igor/js/"
 //FrankensteinConfig.templateRoot = "/frankenstein/application/igor/template"
 //FrankensteinConfig.theme = "gold";
-FrankensteinConfig.root = "http://devcf.glo.texas.gov/frankenstein/application/igor/js/"
-FrankensteinConfig.templateRoot = "http://devcf.glo.texas.gov/frankenstein/application/igor/template"
+FrankensteinConfig.root = "https://jkfrankenstein.netlify.app/frankenstein/application/igor/js/"
+FrankensteinConfig.templateRoot = "https://jkfrankenstein.netlify.app/frankenstein/application/igor/template"
