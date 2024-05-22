893cdafb6a2c37f97447de03ac4f66ab4ac5819a
JK <proemailernumberone@gmail.com>
2024-05-22 15:10:28 -0500

    adding frankenstein

---
+++ frankenstein/application/igor/js/frankenstein-config.js
@@ -0,0 +1,6 @@
+var FrankensteinConfig = {};
+//FrankensteinConfig.root = "/frankenstein/application/igor/js/"
+//FrankensteinConfig.templateRoot = "/frankenstein/application/igor/template"
+//FrankensteinConfig.theme = "gold";
+FrankensteinConfig.root = "http://devcf.glo.texas.gov/frankenstein/application/igor/js/"
+FrankensteinConfig.templateRoot = "http://devcf.glo.texas.gov/frankenstein/application/igor/template"
